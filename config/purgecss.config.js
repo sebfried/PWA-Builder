7fb48086c096251bacced1185f90cce435532f8c
Sebastian Friedrich <dev@sebastianfriedrich.at>
2021-01-11 20:37:11 +0100

    npm update outdated

--- config/purgecss.config.js
+++ config/purgecss.config.js
@@ -1,15 +1,11 @@
 module.exports = {
 	content: ['source/index.pug', 'source/src/index/*.pug', 'source/js/*.js'],
-	css: ['source/css/*.css'],
+    css: ['source/css/*.css'],
 	keyframes: true,
 	fontFace: true,
 	extractors: [
 	  {
-		extractor: class {
-		  static extract(content) {
-			return content.match(/[A-z0-9-:\/]+/g) || []
-		  }
-		},
+        extractor: content => content.match(/[A-z0-9-:\/]+/g) || [],
 		extensions: ['html', 'pug']
 	  }
 	]
--- source/sw.coffee
+++ source/sw.coffee
@@ -6,10 +6,6 @@ filesToCache = [
   '/favicon.ico'
   '/index.html'
   '/img/icon.png'
-  '/app2/manifest.json'
-  '/app2/favicon.ico'
-  '/app2/index.html'
-  '/app2/img/icon.png'
 ]
 
 ### Start the service worker and cache all of the app's content ###
